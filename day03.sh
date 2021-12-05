913578ea32986088dc5b9ead3c20c2f4315b079a
basimkhajwal <basimkhajwal@gmail.com>
2021-12-05 14:27:29 +0000

    Finished Day 3

--- day03.sh
+++ day03.sh
@@ -15,15 +15,25 @@ function bin2dec {
 
 function findNum {
   crit=$1
-  rem=()
+  local rem=()
   while read -r line; do
     rem+=( $line )
-  done
+  done < $file
+  local last
   for (( i=0; i<${#fst}; i++ )); do 
-    if 
-    last=${rem}
-    break
+    local next=()
+    local ones=0
+    for (( j=0; j<$(( ${#rem[@]} )); j++ )); do
+      [[ ${rem[j]:i:1} == 1 ]] && ones=$((ones+1))
+    done
+    bit=$(( ( (ones * 2 >= ${#rem[@]}) ^ (crit == 1) ) ? 1 : 0 ))
+    for (( j=0; j<$(( ${#rem[@]} )); j++ )); do
+      last=${rem[$j]}
+      [[ ${last:i:1} = $bit ]] && next+=( $last )
+    done
+    rem=( ${next[@]+"${next[@]}"} )
   done
+  [[ ${#rem[@]} = 0 ]] && echo $last || echo ${rem[0]}
 }
 
 cnt=()
@@ -39,11 +49,12 @@ while read -r line; do
 done < $file
 
 tot=$(wc -l < $file)
-echo $tot
 a=""
 b=""
 for (( i=0; i<${#fst}; i++ )); do
   a="$a$((${cnt[$i]} > $tot/2 ? 1 : 0))"
   b="$b$((${cnt[$i]} > $tot/2 ? 0 : 1))"
 done
+
 echo $(( $(bin2dec $a) * $(bin2dec $b) ))
+echo $(( $(bin2dec $(findNum 0) ) * $(bin2dec $(findNum 1) ) ))
