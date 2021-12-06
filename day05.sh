0e572f4c861b087ad815e6894459822d0f93e9a2
basimkhajwal <basimkhajwal@gmail.com>
2021-12-06 23:11:53 +0000

    Day 5 Part 2

--- day05.sh
+++ day05.sh
@@ -9,23 +9,18 @@ m=1000
 res=()
 
 while read -r x1 y1 x2 y2; do
-  if [[ $x1 -eq $x2 ]]; then
-    ymin=$(( y1 < y2 ? y1 : y2 ))
-    ymax=$(( y1 < y2 ? y2 : y1 ))
-    for (( i=x1*m+ymin; i<=x1*m+ymax; i++ )); do
+  sx=$(( (x2 > x1) - (x2 < x1) ))
+  sy=$(( (y2 > y1) - (y2 < y1) ))
+  if (( sx == 0 || sy == 0 || useDiag == 1 )); then
+    x=x1 y=y1
+    while (( x != x2+sx || y != y2+sy )); do
+      i=$((x*m+y))
       res[i]=$((${res[i]:-0}+1))
-    done
-  elif [[ $y1 -eq $y2 ]]; then
-    xmin=$(( x1 < x2 ? x1 : x2 ))
-    xmax=$(( x1 < x2 ? x2 : x1 ))
-    for (( i=xmin*m+y1; i<=xmax*m+y1; i+=m )); do
-      res[i]=$((${res[i]:-0}+1))
+      (( x+=sx, y+=sy ))
     done
   fi
 done < <(sed -E 's/(.*),(.*) -> (.*),(.*)/\1 \2 \3 \4/g' $file)
 
 tot=0
-for v in ${res[@]}; do
-  (( tot += v > 1 ? 1 : 0 ))
-done
+for v in ${res[@]}; do (( tot += v > 1 )) ; done
 echo $tot
